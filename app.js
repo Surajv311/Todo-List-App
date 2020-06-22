96a34c0027b066c3c745ff1c769231a5c000b5c2
Surajv <vsuraj2405@gmail.com>
2020-06-22 22:35:38 +0530

    update-6 js file, ejs file

--- app.js
+++ app.js
@@ -5,29 +5,62 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.set('view engine', 'ejs');
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
 
-var today = new Date(); // a module in js
-var currentDay = today.getDay();
-var day = "";
-if(currentDay===6 || currentDay===0){
-day = "Weekend";
-// res.sendFile(__dirname + "/weekend.html");
- res.render('list', {kindOfDay: 'day'}); // kindOfDay in ejs file and day = "Weekend"
- // searching for list.ejs . -> They key value pair foo:FOO is the markdown in the ejs file
+  var today = new Date(); // a module in js
+  var currentDay = today.getDay();
+  var day = "";
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    default:
+    case 1:
+      day = "Monday";
+      break;
+    default:
+    case 2:
+      day = "Tuesday";
+      break;
+    default:
+    case 3:
+      day = "Wednesday";
+      break;
+    default:
+    case 4:
+      day = "Thursday";
+      break;
+    default:
+    case 5:
+      day = "Friday";
+      break;
+    default:
+    case 6:
+      day = "Saturday";
+      break;
+    default:
 
-}else{
-  day = "Weekday";
-  //res.sendFile(__dirname + "/weekday.html");
+  }
+  if (currentDay === 6 || currentDay === 0) {
+    day = "Weekend";
+    // res.sendFile(__dirname + "/weekend.html");
+    // searching for list.ejs . -> They key value pair foo:FOO is the markdown in the ejs file
 
-}
+  } else {
+    day = "Weekday";
+    //res.sendFile(__dirname + "/weekday.html");
 
+  }
+
+  res.render('list', {
+    kindOfDay: 'day'
+  }); // kindOfDay in ejs file and day = "Weekend"
 
 
 
   //res.send("Hello");
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
