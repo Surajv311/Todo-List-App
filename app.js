e5a63a2f996c3e4e1cbba86882ca1d74a07dbe08
Surajv <vsuraj2405@gmail.com>
2020-06-22 21:25:08 +0530

    update-4 js file, ejs file , added views folder

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 //jshint esversion:6
-
+// using ejs templates
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.set('view engine', 'ejs');
 app.get("/", function(req, res){
-  res.send("Hello");
+
+var today = new Date(); // a module in js
+var currentDay = today.getDay();
+var day = "";
+if(currentDay===6 || currentDay===0){
+day = "Weekend";
+  res.sendFile(__dirname + "/weekend.html");
+}else{
+  day = "Weekday";
+  res.sendFile(__dirname + "/weekday.html");
+
+}
+
+
+
+
+  //res.send("Hello");
 });
 
 app.listen(3000, function(){
