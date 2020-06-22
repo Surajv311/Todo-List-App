8d39c009a4e643f90aff479cb3d7297a7e7e7acf
Surajv <vsuraj2405@gmail.com>
2020-06-22 22:30:21 +0530

    update-5 js file, ejs file , added ejs markdown

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ var currentDay = today.getDay();
 var day = "";
 if(currentDay===6 || currentDay===0){
 day = "Weekend";
-  res.sendFile(__dirname + "/weekend.html");
+// res.sendFile(__dirname + "/weekend.html");
+ res.render('list', {kindOfDay: 'day'}); // kindOfDay in ejs file and day = "Weekend"
+ // searching for list.ejs . -> They key value pair foo:FOO is the markdown in the ejs file
+
 }else{
   day = "Weekday";
-  res.sendFile(__dirname + "/weekday.html");
+  //res.sendFile(__dirname + "/weekday.html");
 
 }
 
