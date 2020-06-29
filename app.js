73fa4085a520074f8b3e2b22032bf31aee11ab1e
Surajv <vsuraj2405@gmail.com>
2020-06-30 00:38:26 +0530

    optimising js replaced switch cse with js code

--- app.js
+++ app.js
@@ -8,50 +8,19 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
 
   var today = new Date(); // a module in js
-  var currentDay = today.getDay();
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
+  // replacing the switch case with js code snippet from stackoverflow
 
-    case 1:
-      day = "Monday";
-      break;
 
-    case 2:
-      day = "Tuesday";
-      break;
 
-    case 3:
-      day = "Wednesday";
-      break;
 
-    case 4:
-      day = "Thursday";
-      break;
 
-    case 5:
-      day = "Friday";
-      break;
 
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("error-> day exceeded")
 
-  }
-  // if (currentDay === 6 || currentDay === 0) {
-  //   day = "Weekend";
-  //   // res.sendFile(__dirname + "/weekend.html");
-  //   // searching for list.ejs . -> They key value pair foo:FOO is the markdown in the ejs file
-  //
-  // } else {
-  //   day = "Weekday";
-  //   //res.sendFile(__dirname + "/weekday.html");
-  //
-  // }
+
+
+
+
+
 
   res.render("list", {
     kindOfDay: day
