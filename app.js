18a4d1ca0b190629c1d5f016eb1b52d1e550b262
Surajv <vsuraj2405@gmail.com>
2020-06-30 13:40:35 +0530

    updated js added comment in js and ejs updated ejs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+var item ="";
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
@@ -22,17 +22,27 @@ var day = today.toLocaleDateString("en-US",options)
 // hence now this day is rendered below
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItem: item
   }); // kindOfDay in ejs file and day = "Weekend"
 
-
+// when a post request is triggered on our home route, we'll save the value of newItem
+// in that text box to a variable called item and it will redirect to the home route which then
+// gets us over here and triggers the app.get for our home route.
+// And it will res.render the list template passing in both the kindOfDay as well as the newList
 
   //res.send("Hello");
 });
 
 app.post("/",function(req,res){
-var item = req.body.newItem;
+item = req.body.newItem;
 console.log(item);
+// we could have used this(below) but it would throw error
+// res.render("list", {
+//   newItem: newListItem
+// });
+// hence we redirect it to o=home route and define newListItem there
+res.redirect("/");
 
 })
 
