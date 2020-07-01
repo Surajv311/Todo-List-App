235335859569085fe01f7fa9e8d3c75765cf97b7
Surajv <vsuraj2405@gmail.com>
2020-07-01 20:23:20 +0530

    refractored-2 date.js code

--- date.js
+++ date.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-module.exports.getDate = function(){
+exports.getDate = function(){
 let today = new Date(); // a module in js
 let options = {
   weekday: 'long',
@@ -12,7 +12,7 @@ let options = {
 
 }
 
-module.exports.getDay = function(){
+exports.getDay = function(){
 let today = new Date(); // a module in js
 let options = {
   weekday: 'long',
