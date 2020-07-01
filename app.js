ec09103fe6344ab5bf8ff6eb8b53f8290bdee8aa
Surajv <vsuraj2405@gmail.com>
2020-07-01 15:30:11 +0530

    added work/ route in js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items =["eat" ,"sleep" ,"code"];
+let workItems = [];
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -48,6 +49,15 @@ res.redirect("/");
 
 })
 
+app.get("/work",function(req,res){
+  res.render("list",{listTitle:"Work List" ,newListItems : workItems })
+})
+app.post("/work",function(req,res){
+let item = res.body.newItem;
+workItems.push(newItem);
+res.redirect("/work")
+
+})
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
